3b70127efc4ba17df12415c277351488a3fb6fde
Janina Koivisto <koivistokms@gmail.com>
2023-09-03 15:44:36 +0300

    Lisätty tehtävä

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, Button, FlatList} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+    const [num1,
+        setNum1] = useState('');
+    const [num2,
+        setNum2] = useState('');
+    const [result,
+        setResult] = useState('');
+    const [history,
+        setHistory] = useState([]);
+
+    const handleAdd = () => {
+        const sum = parseInt(num1) + parseInt(num2);
+        const newCalculation = `${num1} + ${num2} = ${sum}`;
+        setResult(`Result: ${sum.toString()}`);
+        setHistory([
+            ...history,
+            newCalculation
+        ]);
+    };
+
+    const handleReset = () => {
+        setNum1('');
+        setNum2('');
+        setResult('');
+        setHistory([]);
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                placeholder="Enter the first number"
+                value={num1}
+                onChangeText={text => setNum1(text)}/>
+            <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                placeholder="Enter the second number"
+                value={num2}
+                onChangeText={text => setNum2(text)}/>
+            <Button title="Add" onPress={handleAdd}/>
+            <Button title="Reset" onPress={handleReset}/>
+            <Text style={styles.result}>{result}</Text>
+            <FlatList
+                data={history}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({item}) => <Text>{item}</Text>}/>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10
+    },
+    result: {
+        marginTop: 20,
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
 });
